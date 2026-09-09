42af718c11ee5a684f106d5614956269992bb47c
Léo Kakali da Fonseca Souza <67524990+kakurokan@users.noreply.github.com>
2026-09-09 14:13:07 +0100

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,34 @@
+class MinStack {
+    Deque<Integer> stack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(val);
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(minStack.peekFirst(), val));
+            stack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty())
+            return;
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peekFirst();
+    }
+
+    public int getMin() {
+        return minStack.peekFirst();
+    }
+}
